0cdda2619d91458a9bf4ccf5e04c8bc059472398
Manuel <hbiaser132@gmail.com>
2024-09-03 10:34:39 -0500

    test

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -2,23 +2,25 @@ import type { Email } from "../../store/current-email";
 
 export const prerender = false;
 
-export const GET = async (props: { request: Request }): Promise<Response> => {
+// export const GET = async (props: { request: Request }): Promise<Response> => {
+//   const { request } = props
+//   const email = new URL(request.url).searchParams.get('email')
+//   const response = await fetch(`https://restmail.net/mail/${email}`)
+//     .then((res) => {
+//       if (!res.ok) throw new Error('Network response was not ok');
+//       return res.json()
+//     })
+//     // .then((res: Email[]) => {
+//     .then((res: any[]) => {
+//       return res.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+//     })
+
+//   return new Response(
+//     JSON.stringify(response),
+//     { headers: { 'content-type': 'application/json' } }
+//   )
+// };
+
+export const GET = async (): Promise<Response> => {
   return new Response('GET request received', { status: 200 })
-
-  const { request } = props
-  const email = new URL(request.url).searchParams.get('email')
-  const response = await fetch(`https://restmail.net/mail/${email}`)
-    .then((res) => {
-      if (!res.ok) throw new Error('Network response was not ok');
-      return res.json()
-    })
-    // .then((res: Email[]) => {
-    .then((res: any[]) => {
-      return res.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-    })
-
-  return new Response(
-    JSON.stringify(response),
-    { headers: { 'content-type': 'application/json' } }
-  )
 };
